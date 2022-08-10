12e0bbe5d50be11e45b2dadc99532ecef398ff88
hayakohe <kohei.hayashi@hrotti.co.jp>
2022-08-10 11:55:09 +0900

    update: 変数への再代入の確認

--- first_test.py
+++ first_test.py
@@ -2,7 +2,16 @@
 def print_arg(something):
     print(something)
 
-
 print_arg(1000)
-
 print_arg("hoge")
+
+def abs(num):
+    return num
+
+arg_count = 100 * -1
+return_value = abs(arg_count)
+print(return_value)
+
+arg_count = 10
+return_value = abs(arg_count)
+print(return_value)
