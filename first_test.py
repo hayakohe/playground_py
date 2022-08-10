3f962012863263a07d90014b9a412afb9de98706
hayakohe <kohei.hayashi@hrotti.co.jp>
2022-08-10 11:58:29 +0900

    update: mathモジュールのインポート、サンプルコード追加

--- first_test.py
+++ first_test.py
@@ -1,3 +1,5 @@
+import math
+
 # 動的型付け言語のため、型指定が不要
 def print_arg(something):
     print(something)
@@ -15,3 +17,7 @@ print(return_value)
 arg_count = 10
 return_value = abs(arg_count)
 print(return_value)
+
+# mathを利用
+square_root = math.sqrt(2.0)
+print(square_root)
